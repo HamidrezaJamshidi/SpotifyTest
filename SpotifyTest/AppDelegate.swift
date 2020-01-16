5f30e829a4064e0d55ba7815fa0dddb61cca9798
HamidrezaJamshidi <hamidreza.jamshidi.2018@gmail.com>
2020-01-16 17:13:32 +0330

    # App icon changed

---
+++ SpotifyTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SpotifyTest
+//
+//  Created by Hamidreza Jamshidi on 10/26/1398 AP.
+//  Copyright © 1398 AP JDeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
