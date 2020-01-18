e5d455b7f2256f54aaeff18c2be0e53fa532c23f
HamidrezaJamshidi <hamidreza.jamshidi.2018@gmail.com>
2020-01-18 06:42:30 +0330

    no message

--- SpotifyTest/Configs/TokenProvider.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TokenProvider.swift
-//  SpotifyTest
-//
-//  Created by Hamidreza Jamshidi on 10/27/1398 AP.
-//  Copyright © 1398 AP JDeveloper. All rights reserved.
-//
-
-import Foundation
